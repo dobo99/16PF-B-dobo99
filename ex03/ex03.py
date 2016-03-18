08d9f03f79dae9e4b9d79b8ec0c572699236ad0a
CAD Client <CAD Client>
2016-03-18 15:18:45 +0900

    added comments

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -1,17 +1,22 @@
+#-*-coding:cp949
 print "I will now count my chickens:"
 
 print "Hens", 25 + 30 / 6
+# * and / is caculating faster than +,-
 print "Roosters", 100 - 25 * 3 % 4
+# % is kind of dividing, but print remaining part
 
 print "Now I will count the eggs:"
 
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+# 1/4 is 0.25. but fraction part is dropped when printing
 print "Is it true that 3+2<5-7?"
 
 print 3 + 2 < 5 - 7
+# when I printing < or >, it shows true or false
 
 print "What is 3+2?", 3 + 2
+# equations inside of "" are just word. Outside of "" is real equation
 print "What is 5-7?", 5 - 7
 
 print "Oh, that's why it's False."
