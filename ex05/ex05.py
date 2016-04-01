639c1788a43a27ddd7d14528ffbc454527f98043
CAD Client <CAD Client>
2016-04-01 14:59:22 +0900

    study drills 1

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,18 +1,25 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 #inches
-my_weight = 180 #lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Lee'
+age = 25
+height_cm = 175
+weight_kg = 70
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height_cm
+print "He's %d pounds heavy." % weight_kg
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 #this line is tricky, try to get exactly right
 print "If I add %d, %d, and %d I get %d." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight)
+    age, height_cm, weight_kg, age + height_cm + weight_kg)
+print "I'll convert centimeters to inches, kilograms to pounds"
+print '1 centimeter = 0.39 inch'
+print '1 kilogram = 2.2 pound'
+print 'converting %d cm to %d in' % (height_cm, height_cm*0.39)
+print 'converting %d kg to %d lbs' % (weight_kg, weight_kg*2.2)
+print "What is %r?"
+print '%r' % 'print this no matter what'
