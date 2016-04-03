f4473892c41b3dcdaadec2df00cd7c6d1ffe8638
dobo99 <engjs77@naver.com>
2016-04-03 15:47:32 +0900

    modified error and added comments

--- ex07/ex07.py
+++ ex07/ex07.py
@@ -1,7 +1,9 @@
 print "Mary had a little lamb"
-print "Its fleece was White aas %s." % 'snow'
+print "Its fleece was White as %s." % 'snow'
+# I use string format code %s to put in the string
 print "And everywhere that Mary went."
 print "." * 10 # what'd that do?
+# I can multiple wards many times
 
 end1 = "C"
 end2 = "h"
@@ -15,7 +17,10 @@ end9 = "r"
 end10 = "g"
 end11 = "e"
 end12 = "r"
+# I use equal code(=) when change the string to another string
 
 # watch that comma at the end. try removing it to see what happens
 print end1 + end2 + end3 + end4 + end5 + end6,
+# I can plus many strings
 print end7 + end8 + end9 + end10 + end11 + end12
+# comma(,) can connect two lines then make one line
