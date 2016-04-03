be1ccfe327679462cb0e36dde9e01bdc3045ccc0
dobo99 <engjs77@naver.com>
2016-04-03 16:22:15 +0900

    added comments

--- ex09/ex09.py
+++ ex09/ex09.py
@@ -2,6 +2,7 @@
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# code'\n' can change the line
 
 print "Here are the days: ", days
 print "Here are the months: ", months
@@ -12,3 +13,4 @@ With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 """
+# """.""" can fix the inner string's line
