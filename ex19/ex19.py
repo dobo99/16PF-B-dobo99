7cdebc644ff13291ff49cefe3cf632feb071da2d
CAD Client <CAD Client>
2016-04-15 15:58:29 +0900

    edit

--- ex19/ex19.py
+++ ex19/ex19.py
@@ -1,6 +1,7 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print "You have %d cheese!" % cheese_count
     print "You have %d boxes of crackers!" % boxes_of_crackers
+    print "Man that's enough for a party"
     print "get a blanket.\n"
 
 
