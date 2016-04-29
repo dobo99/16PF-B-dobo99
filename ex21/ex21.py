acc3c822e8080d6ec29c3fa032fa8483bfc29162
CAD Client <CAD Client>
2016-04-29 15:39:08 +0900

    added comment

--- ex21/ex21.py
+++ ex21/ex21.py
@@ -1,6 +1,8 @@
 def add(a, b):
     print "ADDING %d + %d" % (a, b)
     return a + b
+# define calculation order. print formula and return answer
+# same process about blow three define
 
 def subtract (a, b):
     print "SUBTRACTING %d - %d" % (a, b)
@@ -28,5 +30,6 @@ print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
 print "Here is a puzzle."
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+# printing process is inside to outside. print inside first
 
 print "That becomes: ", what, "Can you do me a hand?"
