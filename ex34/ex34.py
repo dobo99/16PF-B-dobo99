32e47921b202c721e6004a2d5b246a7a52994680
dobo99 <engjs77@naver.com>
2016-05-28 09:39:48 +0900

    do ex34

---
+++ ex34/ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+tiger = animals[1]
+penguin = animals[2]
+zebra = animals[3]
+# we can select what in the list with number. numbering left to right start with 0.
+
+print("bear = %s" % bear)
+print("tiger = %s" % tiger)
+print("penguin = %s" % penguin)
+print("zebra = %s" % zebra)
